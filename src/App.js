061eacc08780d4745286dd72a65438537db362c3
Lea Hughes <lahughesmusic@gmail.com>
2020-02-19 19:51:27 -0600

    add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Grid from "./components/grid";
+import Play from './components/track';
 
 import "./App.css";
 
 function App() {
-  return <Grid></Grid>;
+  return (
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={Grid} />
+        <Route exact path="/" component={Play} />
+      </Fragment>
+    </Router>
+  )
+
+
 }
 
 export default App;
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Play from './track';
 
 function Grid() {
-  let [keySig, setKeySig] = useState("C");
-  useEffect(() => console.log(keySig));
+  const [chord1, setChord1] = useState("C");
+  const [chord2, setChord2] = useState('C');
+  const [chord3, setChord3] = useState('C');
+  const [chord4, setChord4] = useState('C');
+
   const styles = {
     border: "2px solid darkblue",
     height: "150px",
-    width: "25%",
-    marginTop: "100px"
+    width: "20%",
+    marginTop: "100px",
+
+    float: 'left'
   };
   const dropdownStyles = {
     border: "px solid darkblue",
@@ -19,73 +25,166 @@ function Grid() {
     position: "absolute",
     marginLeft: "7%"
   };
+  const left = {
+
+    marginLeft: "50px"
+  };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div style={styles} className="col-">
-          <h1 style={keyStyle}>{keySig}</h1>
-          <h2>1.</h2>
+    <div style={left}>
 
-          <label for="key">KEY </label>
+      <div style={styles} className="col-">
+        <h1 style={keyStyle}>{chord1}</h1>
+        <h2>1.</h2>
 
-          <select
-            style={dropdownStyles}
-            id="key"
-            onChange={e => setKeySig(e.targeValue)}
-          >
-            <option value="C">C</option>
-            <option value="dm">dm</option>
-            <option value="em">em</option>
-            <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="am">am</option>
-          </select>
-        </div>
-        <div style={styles} className="col-">
-          <h1 style={keyStyle}>C</h1>
-          <h2>2.</h2>
-          <label for="key">KEY </label>
+        <label for="key">KEY </label>
 
-          <select style={dropdownStyles} id="key">
-            <option value="C">C</option>
-            <option value="dm">dm</option>
-            <option value="em">em</option>
-            <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="am">am</option>
-          </select>
-        </div>
-        <div style={styles} className="col-">
-          <h1 style={keyStyle}>C</h1>
-          <h2>3.</h2>
-          <label for="key">KEY </label>
-
-          <select style={dropdownStyles} id="key">
-            <option value="C">C</option>
-            <option value="dm">dm</option>
-            <option value="em">em</option>
-            <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="am">am</option>
-          </select>
-        </div>
-        <div style={styles} className="col-">
-          <h1 style={keyStyle}>C</h1>
-          <h2>4.</h2>
-          <label for="key">KEY </label>
-
-          <select style={dropdownStyles} id="key">
-            <option value="C">C</option>
-            <option value="dm">dm</option>
-            <option value="em">em</option>
-            <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="am">am</option>
-          </select>
-        </div>
+        <select
+          style={dropdownStyles}
+          id="key"
+          onChange={e => setChord1(e.target.value)}
+        >
+          <option value="C">C</option>
+          <option value="dm">dm</option>
+          <option value="em">em</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="am">am</option>
+        </select>
       </div>
+
+      <div style={styles} className="col-">
+        <h1 style={keyStyle}>{chord2}</h1>
+        <h2>2.</h2>
+
+        <label for="key">KEY </label>
+
+        <select
+          style={dropdownStyles}
+          id="key"
+          onChange={e => setChord2(e.target.value)}
+        >
+          <option value="C">C</option>
+          <option value="dm">dm</option>
+          <option value="em">em</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="am">am</option>
+        </select>
+      </div>
+
+      <div style={styles} className="col-">
+        <h1 style={keyStyle}>{chord3}</h1>
+        <h2>3.</h2>
+
+        <label for="key">KEY </label>
+
+        <select
+          style={dropdownStyles}
+          id="key"
+          onChange={e => setChord3(e.target.value)}
+        >
+          <option value="C">C</option>
+          <option value="dm">dm</option>
+          <option value="em">em</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="am">am</option>
+        </select>
+      </div>
+
+      <div style={styles} className="col-">
+        <h1 style={keyStyle}>{chord4}</h1>
+        <h2>4.</h2>
+
+        <label for="key">KEY </label>
+
+        <select
+          style={dropdownStyles}
+          id="key"
+          onChange={e => setChord4(e.target.value)}
+        >
+          <option value="C">C</option>
+          <option value="dm">dm</option>
+          <option value="em">em</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="am">am</option>
+        </select>
+      </div>
+
+
+
     </div>
+
+    // <div className="container">
+    //   <div className="row">
+    //     <div style={styles} className="col-">
+    //       <h1 style={keyStyle}>{this.chord}</h1>
+    //       <h2>1.</h2>
+
+    //       <label for="key">KEY </label>
+
+    //       <select
+    //         style={dropdownStyles}
+    //         id="key"
+    //         onChange={e => setChord(e.target.value)}
+    //       >
+    //         <option value="C">C</option>
+    //         <option value="dm">dm</option>
+    //         <option value="em">em</option>
+    //         <option value="F">F</option>
+    //         <option value="G">G</option>
+    //         <option value="am">am</option>
+    //       </select>
+    //     </div>
+    //     <div style={styles} className="col-">
+    //       <h1 style={keyStyle}>{this.chord}</h1>
+    //       <h2>2.</h2>
+    //       <label for="key">KEY </label>
+
+    //       <select style={dropdownStyles} id="key"
+    //         onChange={e => setChord(e.target.value)}>
+    //         <option value="C">C</option>
+    //         <option value="dm">dm</option>
+    //         <option value="em">em</option>
+    //         <option value="F">F</option>
+    //         <option value="G">G</option>
+    //         <option value="am">am</option>
+    //       </select>
+    //     </div>
+    //     <div style={styles} className="col-">
+    //       <h1 style={keyStyle}>{this.chord}</h1>
+    //       <h2>3.</h2>
+    //       <label for="key">KEY </label>
+
+    //       <select style={dropdownStyles} id="key"
+    //         onChange={e => setChord(e.target.value)}>
+    //         <option value="C">C</option>
+    //         <option value="dm">dm</option>
+    //         <option value="em">em</option>
+    //         <option value="F">F</option>
+    //         <option value="G">G</option>
+    //         <option value="am">am</option>
+    //       </select>
+    //     </div>
+    //     <div style={styles} className="col-">
+    //       <h1 style={keyStyle}>{this.chord}</h1>
+    //       <h2>4.</h2>
+    //       <label for="key">KEY </label>
+
+    //       <select style={dropdownStyles} id="key"
+    //         onChange={e => setChord(e.target.value)} ß>
+    //         <option value="C">C</option>
+    //         <option value="dm">dm</option>
+    //         <option value="em">em</option>
+    //         <option value="F">F</option>
+    //         <option value="G">G</option>
+    //         <option value="am">am</option>
+    //       </select>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
